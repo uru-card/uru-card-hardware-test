3d89545a7616b080afbe9f04c2adc82198023313
Andrey Ovcharov <andrew.ovcharov@gmail.com>
2020-10-19 19:19:55 +0200

    Test utility code

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,27 @@
-#include<Arduino.h>
+#include <Arduino.h>
 #include <Wire.h>
 
 #include <Adafruit_GFX.h>
-#include <Adafruit_MPR121.h>
 #include <Adafruit_SSD1306.h>
+
+#include <Adafruit_MPR121.h>
+
+#include <SparkFun_ATECCX08a_Arduino_Library.h> //Click here to get the library: http://librarymanager/All#SparkFun_ATECCX08a
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
 
+bool screen_available = false;
+
 Adafruit_MPR121 cap = Adafruit_MPR121();
+
+bool keyboard_available = false;
+
+ATECCX08A atecc;
+
+bool security_available = false;
 
 void setup()
 {
@@ -19,34 +30,48 @@ void setup()
 
     delay(2000);
 
-    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    if (display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
     {
-        Serial.println(F("SSD1306 allocation failed"));
-        while (1)
-            ;
+        screen_available = true;
     }
-    Serial.println("SSD1306 found!");
 
-    display.clearDisplay();
-    display.setTextSize(3);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0, 0);
-    display.println("URUCard");
-    display.display();
-
-    // Default address is 0x5A, if tied to 3.3V its 0x5B
-    // If tied to SDA its 0x5C and if SCL then 0x5D
-    if (!cap.begin(0x5A))
+    if (cap.begin(0x5A))
     {
-        Serial.println("MPR121 not found, check wiring?");
-        while (1)
-            ;
+        keyboard_available = true;
     }
-    Serial.println("MPR121 found!");
+
+    security_available = atecc.begin();
+}
+
+void print_status(const char *label, const char *value)
+{
+    Serial.println(label);
+    Serial.println(value);
+
+    if (screen_available)
+    {
+        display.clearDisplay();
+        display.setTextSize(2);
+        display.setTextColor(SSD1306_WHITE);
+        display.setCursor(0, 0);
+        display.println(label);
+        display.setCursor(0, 16);
+        display.println(value);
+        display.display();
+    }
 }
 
 void loop()
 {
-    delay(1000);
+    print_status("URU Card", "HW Test");
+    delay(3000);
+
+    print_status("SSD1306", screen_available ? "Ok" : "Error!");
+    delay(3000);
+
+    print_status("MPR121", keyboard_available ? "Ok" : "Error!");
+    delay(3000);
+
+    print_status("ATECC508A", security_available ? "Ok" : "Error!");
+    delay(3000);
 }
